0ef5c79a62beac1089fbb74cd353fefcedcf9524
1zumiii <izumiii.reflection@gmail.com>
2026-05-04 21:30:05 +1000

    Format code in PreviewWatermarkService for consistency and readability

--- PhantomStamp/Services/PreviewWatermarkService.swift
+++ PhantomStamp/Services/PreviewWatermarkService.swift
@@ -8,14 +8,14 @@
 import UIKit
 
 final class PreviewWatermarkService: WatermarkServiceProtocol {
-  func embedWatermark(into image: UIImage, text: String) async throws -> UIImage {
-    try await Task.sleep(nanoseconds: AppConstants.Watermark.mockEmbedDelayNanoseconds)
-    return image
-  }
+    func embedWatermark(into image: UIImage, text: String) async throws -> UIImage {
+        try await Task.sleep(nanoseconds: AppConstants.Watermark.mockEmbedDelayNanoseconds)
+        return image
+    }
 
-  func extractWatermark(from image: UIImage) async throws -> String {
-    try await Task.sleep(nanoseconds: AppConstants.Watermark.mockExtractDelayNanoseconds)
-    _ = image
-    return AppConstants.Watermark.mockExtractResultText
-  }
+    func extractWatermark(from image: UIImage) async throws -> String {
+        try await Task.sleep(nanoseconds: AppConstants.Watermark.mockExtractDelayNanoseconds)
+        _ = image
+        return AppConstants.Watermark.mockExtractResultText
+    }
 }
